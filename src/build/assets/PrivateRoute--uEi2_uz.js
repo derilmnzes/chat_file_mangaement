695c45d114f58ebbeeeb53fb35803a89127939ff
derilmnzes <derilmenezes02@gmail.com>
2023-12-23 16:28:00 +0400

    new changes

--- src/build/assets/PrivateRoute--uEi2_uz.js
+++
@@ -1,1 +0,0 @@
-import{i as s,j as t,a7 as a,a8 as u}from"./index-U3fbFpKl.js";function i(){return s(e=>e.user.isAuth)?t.jsx(a,{}):t.jsx(u,{to:"/user/auth"})}export{i as default};
---
+++ src/build/assets/PrivateRoute-4HbrsSSY.js
@@ -0,0 +1,1 @@
+import{u as s,j as t,a5 as u,a6 as a}from"./index-hPQDWh1f.js";function i(){return s(e=>e.user.isAuth)?t.jsx(u,{}):t.jsx(a,{to:"/user/auth"})}export{i as default};
