d4be08c7444e7f11ff3ff4d8183b75e66e43fc22
derilmnzes <derilmenezes02@gmail.com>
2023-12-23 18:41:09 +0400

    new chnages

--- src/build/assets/PrivateRoute-4HbrsSSY.js
+++
@@ -1,1 +0,0 @@
-import{u as s,j as t,a5 as u,a6 as a}from"./index-hPQDWh1f.js";function i(){return s(e=>e.user.isAuth)?t.jsx(u,{}):t.jsx(a,{to:"/user/auth"})}export{i as default};
---
+++ src/build/assets/PrivateRoute-TNv7FwCq.js
@@ -0,0 +1,1 @@
+import{u as s,j as t,a6 as u,a7 as a}from"./index-R8GyF8uZ.js";function i(){return s(e=>e.user.isAuth)?t.jsx(u,{}):t.jsx(a,{to:"/user/auth"})}export{i as default};
